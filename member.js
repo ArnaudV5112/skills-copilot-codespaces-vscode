4cd3e074e351e14849a88701feb2d42ae7735a48
ArnaudV5112 <166705248+ArnaudV5112@users.noreply.github.com>
2024-04-18 15:32:35 +0000

    Second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMembers() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/members.html',
+        controller: 'SkillsMembersController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            members: '='
+        }
+    };
+}
